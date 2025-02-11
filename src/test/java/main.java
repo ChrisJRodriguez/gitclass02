d1798bdeeee8ab957b5f173e6795229798e9d8b2
ChrisJRodriguez <rodriguezcj293@gmail.com>
2025-02-11 18:43:03 -0500

    first commit

---
+++ src/test/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("heloo world");
+        System.out.println("this is the project version 1");
+    }
+}
+//done with version 1
